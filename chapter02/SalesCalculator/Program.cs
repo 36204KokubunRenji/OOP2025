dbf7b4133aea024caf45333ddfb303cbf2e1e119
KokubunRenji <oibc36204@ojs.ac.jp>
2025-05-09 15:59:25 +0900

    暗黙の型指定を使用

--- chapter02/SalesCalculator/Program.cs
+++ chapter02/SalesCalculator/Program.cs
@@ -1,8 +1,8 @@
 ﻿namespace SalesCalculator {
     internal class Program {
         static void Main(string[] args) {
-            SalesCounter sales = new SalesCounter(@"data\sales.csv");
-            IDictionary<string, int> amountsPerStore = sales.GetPerStoreSales();
+            var sales = new SalesCounter(@"data\sales.csv");
+            var amountsPerStore = sales.GetPerStoreSales();
             foreach (KeyValuePair<string, int> obj in amountsPerStore) {
                 Console.WriteLine($"{obj.Key} {obj.Value}");
             }
--- chapter02/SalesCalculator/SalesCounter.cs
+++ chapter02/SalesCalculator/SalesCounter.cs
@@ -30,14 +30,14 @@ namespace SalesCalculator {
         //売り上げデータを読み込み、Saleオブジェクトのリストを返す
         public static IEnumerable<Sale> ReadSales(string filePath) {
             //売り上げデータを入れるリストオブジェクトを生成
-            List<Sale> sales = new List<Sale>();
+           var sales = new List<Sale>();
             //ファイルを一気に読み込み
-            string[] lines = File.ReadAllLines(filePath);
+            var lines = File.ReadAllLines(filePath);
             //読み込んだ行数分繰り返し
-            foreach (string line in lines) {
+            foreach (var line in lines) {
                 string[] items = line.Split(',');
                 //Saleオブジェクトを生成
-                Sale sale = new Sale() {
+                var sale = new Sale() {
                     ShopName = items[0],
                     ProductCategory = items[1],
                     Amount = int.Parse(items[2])
