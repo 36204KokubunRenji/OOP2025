58d4a46b3a3866b506f532917a723b011dc617e9
KokubunRenji <oibc36204@ojs.ac.jp>
2025-05-13 11:12:20 +0900

    2.1.1完成

---
+++ chapter02/演習問題/Execise01/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execise01{
+    //2.1.1
+    public class Song {
+        public string Title { get; set; }
+        public string ArtistName { get; set; }
+        public int Length { get; set; }
+
+
+        //2.1.2
+        public Song(string Title, string ArtistName, int Length);
+           
+
+    }
+}
