fa06f98c679ce7009ff16116de7a12139ec568b0
KokubunRenji <oibc36204@ojs.ac.jp>
2025-05-16 14:56:41 +0900

    P71

--- chapter03/Section01/Program.cs
+++ chapter03/Section01/Program.cs
@@ -2,25 +2,22 @@
     internal class Program {
 
         static void Main(string[] args) {
+            var cities = new List<string> {
+    "Tokyo",
+    "New Delhi",
+    "Bangkok",
+    "London",
+    "Paris",
+    "Berlin",
+    "Canberra",
+    "Hong Kong",
+            };
+            
+            var lowerList = cities.ConvertAll(s => s.ToUpper());
+            lowerList.ForEach(s => Console.WriteLine(s));
 
-           // Console.WriteLine("カウントしたい数値：");
-           // int num = int.Parse(Console.ReadLine());
 
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-
-            Console.WriteLine(Count(numbers,  n => 5 <= n && n < 10 ) );
         }
-
-            static int Count(int[] numbers, Predicate<int> judge)  {
-                var count = 0;
-                foreach (var n in numbers) {
-                    //引数で受け取ったメソッドを呼び出す
-                    if ( judge(n) == true) {
-                        count++;
-                    }
-                }
-                return count;
-            }
 
            
         
