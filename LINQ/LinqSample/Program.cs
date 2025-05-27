bcf5241782b48f8889bdfbc58684c9bdd4b9a1b3
KokubunRenji <oibc36204@ojs.ac.jp>
2025-05-27 11:15:38 +0900

    演習問題

---
+++ LINQ/LinqSample/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LinqSample {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var numbers = Enumerable.Range(1, 100);
+
+            //合計値を出力
+            Console.WriteLine(numbers.Sum());
+            //平均値を出力
+            Console.WriteLine(numbers.Average());
+
+            Console.WriteLine(numbers.Where(n => n % 8 == 0).Sum());
+            //Console.WriteLine(numbers.Where(n => n % 8 == 0).Max());
+
+            //foreach (var num in numbers) {
+            //    Console.WriteLine(num);
+            //}
+            
+
+        }
+    }
+}
