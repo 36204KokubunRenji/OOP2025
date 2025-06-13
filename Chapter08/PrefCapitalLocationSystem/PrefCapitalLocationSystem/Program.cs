6885370a2590a32aa425e26f6dd8341ee9828e8d
KokubunRenji <oibc36204@ojs.ac.jp>
2025-06-13 14:28:42 +0900

    完成（写し）

--- Chapter08/PrefCapitalLocationSystem/PrefCapitalLocationSystem/Program.cs
+++ Chapter08/PrefCapitalLocationSystem/PrefCapitalLocationSystem/Program.cs
@@ -21,20 +21,12 @@
 
                 //既に都道府県が登録されているか？
                 //ヒント：ContainsKeyを使用して調べる
-                if (pref.ContainsKey()) {
-
+                if (prefOfficeDict.ContainsKey(pref)) {
+                    Console.WriteLine("上書きしますか？(Y/N)");
+                    if (Console.ReadLine() == "N")continue;
                 }
-
-                //登録済みなら確認して上書き処理、上書きしない場合はもう一度都道府県の入力…①へ
-                //ヒント：Console.WriteLine("上書きしますか？(Y/N)");
-
-                //*　ここに入力　*******************//
-
-
                 //県庁所在地登録処理
-
-                //*　ここに入力　*******************//
-
+                prefOfficeDict[pref] = prefCaptalLocation ?? "** 未入力 **";
 
                 Console.WriteLine();//改行
             }
@@ -53,7 +45,7 @@
 
 
                     case "9"://無限ループを抜ける
-                        //*　ここに入力　*******************//
+                        endFlag = true;
                         break;
                 }
             }
@@ -73,14 +65,19 @@
 
         //一覧表示処理
         private static void allDisp() {
-
-            //*　ここに入力　*******************//
+            foreach (var item in prefOfficeDict) {
+                Console.WriteLine($"{item.Key}の県庁所在地は {item.Value}市です。");
+            }
+           
         }
 
         //検索処理
         private static void searchPrefCaptalLocation() {
             Console.Write("都道府県:");
             String? searchPref = Console.ReadLine();
+            if (searchPref is null) return;
+            Console.WriteLine(searchPref + "の県庁所在地は" + prefOfficeDict[searchPref] + "です。");
+            
         }
     }
 }
