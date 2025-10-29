650abd62e7b72f5d193f32bfccd2e43309d48a23
KokubunRenji <oibc36204@ojs.ac.jp>
2025-10-29 11:16:44 +0900

    1-5,1-6完成

--- chapter13/Section01/Program.cs
+++ chapter13/Section01/Program.cs
@@ -1,13 +1,11 @@
 ﻿namespace Section01 {
     internal class Program {
         static void Main(string[] args) {
-            var selected = Library.Books
-                .GroupBy(b => b.PublishedYear)
-                .Select(group => group.MaxBy(b => b.Price))
-                .OrderBy(b => b!.PublishedYear);
-                
-            foreach (var book in selected) {
-                Console.WriteLine($"{book!.PublishedYear}年 {book!.Title} ({book!.Price})");
+            var books = Library.Books.Join(Library.Categories, book => book.CategoryId, Category => Category.Id, (book, category)
+                => new { book.Title, Category = category.Name, book.PublishedYear });
+
+            foreach (var book in books) {
+                Console.WriteLine($"{book.Title},{book.Category},{book.PublishedYear}");
             }
         }
     }
--- chapter13/演習問題/Exercise01/Program.cs
+++ chapter13/演習問題/Exercise01/Program.cs
@@ -24,19 +24,40 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_3() {
-            
+            //Console.WriteLine("2020:" + Library.Books.Where(a => a.PublishedYear == 2020).Count());
+            //Console.WriteLine("2021:" + Library.Books.Where(a => a.PublishedYear == 2021).Count());
+            //Console.WriteLine("2022:" + Library.Books.Where(a => a.PublishedYear == 2022).Count());
+            //Console.WriteLine("2023:" + Library.Books.Where(a => a.PublishedYear == 2023).Count());
+
+            var results = Library.Books.GroupBy(b => b.PublishedYear).OrderBy(b => b.Key).Select(b => new { PublishedYear = b.Key, Count = b.Count() });
+            foreach (var item in results) {
+                Console.WriteLine($"{item.PublishedYear} : {item.Count}");
+            }
         }
 
         private static void Exercise1_4() {
-            
+            var books = Library.Books.OrderByDescending(c => c.PublishedYear).ThenByDescending(c => c.Price);
+            foreach (var item in books) {
+                Console.WriteLine($"{item.PublishedYear} : {item.Price} : {item.Title}");
+            }
         }
 
         private static void Exercise1_5() {
-            
+            var categoryID = Library.Books.Where(d => d.PublishedYear == 2023).Join(Library.Categories
+                , book => book.CategoryId, category => category.Id, (book, category) => category.Name).Distinct();
+            foreach (var name in categoryID) {
+                Console.WriteLine(name);
+            }
         }
 
         private static void Exercise1_6() {
-            
+            var groups = Library.Categories.GroupJoin(Library.Books, e => e.Id, f => f.CategoryId, (c, books) => new { Category = c.Name, Books = books });
+            foreach (var group in groups) {
+                Console.WriteLine($"# {group.Category}");
+                foreach (var book in group.Books) {
+                    Console.WriteLine($"{book.Title} ({book.PublishedYear}年)");
+                }
+            }
         }
 
         private static void Exercise1_7() {
