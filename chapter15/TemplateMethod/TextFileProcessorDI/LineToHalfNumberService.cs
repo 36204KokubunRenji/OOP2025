3c45eb03837fcf8e82c6ace0730416d0ff9d7299
KokubunRenji <oibc36204@ojs.ac.jp>
2025-11-17 15:56:34 +0900

    15.3

---
+++ chapter15/TemplateMethod/TextFileProcessorDI/LineToHalfNumberService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileProcessorDI {
+    public class LineToHalfNumberService : ITextFileService {
+        public void Execute(string line) {
+
+        }
+        public void Initialize(string fname) {
+        }
+
+        public void Terminate() {
+        }
+    }
+}
